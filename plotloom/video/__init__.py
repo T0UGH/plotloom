30bdda2550fd1e8850ff30cde42c6daa2f703835
wangguiping.1 <wangguiping.1@bytedance.com>
2026-04-30 16:32:08 +0800

    feat: add video request validation contracts

---
+++ plotloom/video/__init__.py
@@ -0,0 +1,18 @@
+from plotloom.video.capabilities import capabilities_for, validate_request
+from plotloom.video.types import (
+    PlotloomVideoRequest,
+    ValidationIssue,
+    ValidationResult,
+    VideoAdapterCapabilities,
+    VideoMode,
+)
+
+__all__ = [
+    "PlotloomVideoRequest",
+    "ValidationIssue",
+    "ValidationResult",
+    "VideoAdapterCapabilities",
+    "VideoMode",
+    "capabilities_for",
+    "validate_request",
+]
